02451c73733b5e11662782852369222797b58d1a
JungYouhwan <qjdxjd0643@naver.com>
2024-05-21 17:35:07 +0900

    05-21 오후

--- groupware/src/main/java/com/samjo/app/work/web/WorkControll.java
+++ groupware/src/main/java/com/samjo/app/work/web/WorkControll.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.samjo.app.work.service.WorkPageDTO;
 import com.samjo.app.work.service.WorkSearchVO;
