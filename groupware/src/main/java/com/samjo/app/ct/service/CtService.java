31ab837d896bb0789a37d83046f1acde00b2b10f
EyeOfNoob <crescentbear@naver.com>
2024-05-29 01:43:02 +0900

    0529 AM UPDATE

--- groupware/src/main/java/com/samjo/app/ct/service/CtService.java
+++ groupware/src/main/java/com/samjo/app/ct/service/CtService.java
@@ -1,6 +1,7 @@
 package com.samjo.app.ct.service;
 
 import java.util.List;
+import java.util.Map;
 
 import com.samjo.app.common.service.SearchVO;
 import com.samjo.app.solmodule.service.ModuleVO;
@@ -15,4 +16,6 @@ public interface CtService {
 	public CtVO ctInfo(int ctNO);
 	//모듈목록 조회
 	public List<ModuleVO> modList();
+	//계약 수정처리
+	public Map<String, Object> ctUpdate(CtVO ctVO, String[] modIds);
 }
--- groupware/src/main/java/com/samjo/app/ct/web/CtController.java
+++ groupware/src/main/java/com/samjo/app/ct/web/CtController.java
@@ -1,5 +1,6 @@
 package com.samjo.app.ct.web;
 
+import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.samjo.app.common.service.SearchVO;
 import com.samjo.app.ct.service.CtDTO;
@@ -20,8 +23,8 @@ public class CtController {
 
 	@Autowired
 	CtService ctservice;
-	
-	//계약 전체조회
+
+	// 계약 전체조회
 	@GetMapping("sol/ctList")
 	public String ctListPage(SearchVO searchVO, Model model) {
 		if (searchVO.getPage() <= 0) {
@@ -30,19 +33,16 @@ public class CtController {
 		if (searchVO.getCtSort() == null || searchVO.getCtSort().trim().isEmpty()) {
 			searchVO.setCtSort("ct_no");
 		}
-		System.out.println(searchVO);
 		List<CtVO> list = ctservice.ctList(searchVO);
 		model.addAttribute("list", list);
 		CtDTO ctDTO = new CtDTO(searchVO.getPage(), ctservice.count(searchVO));
 		model.addAttribute("CtDTO", ctDTO);
 		return "solution/ct/ctList";
 	}
-	
-	//계약 조회페이지 검색/페이징 처리
+
+	// 계약 조회페이지 검색/페이징 처리
 	@PostMapping("sol/viewCtList")
 	public String viewCtListPage(SearchVO searchVO, Model model) {
-		System.out.println("searchVO: "+searchVO);
-		System.out.println("startDay : "+ searchVO.getCtStart());
 		if (searchVO.getPage() <= 0) {
 			searchVO.setPage(1);
 		}
@@ -55,22 +55,34 @@ public class CtController {
 		model.addAttribute("CtDTO", ctDTO);
 		return "solution/ct/ctList :: #ctTable";
 	}
-	
-	//계약 상세조회
+
+	// 계약 상세조회
 	@GetMapping("sol/ctInfo/{ctNo}")
-	public String ctInfoPage(@PathVariable int ctNo,Model model) {
+	public String ctInfoPage(@PathVariable int ctNo, Model model) {
 		CtVO ctVO = ctservice.ctInfo(ctNo);
 		model.addAttribute("ctVO", ctVO);
 		return "solution/ct/ctInfo";
 	}
-	
-	//계약 수정 화면
+
+	// 계약 수정 화면
 	@GetMapping("sol/ctUpdate/{ctNo}")
-	public String ctUpdatePage(@PathVariable int ctNo, Model model){
+	public String ctUpdatePage(@PathVariable int ctNo, Model model) {
 		List<ModuleVO> modlist = ctservice.modList();
 		model.addAttribute("modlist", modlist);
 		CtVO ctVO = ctservice.ctInfo(ctNo);
 		model.addAttribute("ctVO", ctVO);
 		return "solution/ct/ctUpdate";
 	}
+
+	// 계약 수정 처리
+	@PostMapping("sol/ctUpdateProcess/{ctNo}")
+	@ResponseBody
+	public String ctUpdateProcess(@PathVariable int ctNo, CtVO ctVO, String[] modIds, Model model) {
+		//ctservice.modUpdate(ctVO, modIds);
+		System.out.println("CT : " + ctVO);
+		System.out.println("modIds: " + Arrays.toString(modIds));
+		ctservice.ctUpdate(ctVO, modIds);
+		return "Success";
+	}
+	
 }
